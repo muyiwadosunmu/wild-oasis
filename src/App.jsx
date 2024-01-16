0fc97961e76a74c4322bacd75f7e990333429a4e
Dosunmu Oluwamuyiwa <oluwamuyiwadosunmu@gmail.com>
2024-01-16 10:23:11 +0100

    Some basic refractor

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,28 @@
 import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
 
 const H1 = styled.h1`
   font-size: 30px;
   font-weight: 600;
 `;
-const Input = styled.input`
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  padding: 0.8rem 1.2rem;
-`;
 
 const StyledApp = styled.div`
-  background-color: orangered;
+  background-color: #eeeceb;
   padding: 20px;
 `;
 
 function App() {
   return (
-    <StyledApp>
-      <H1>The Wild Oasis</H1>
-      <Input placeholder="Fill in the form" />
-    </StyledApp>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Button>Click Me</Button>
+        <H1>The Wild Oasis</H1>
+        <Input placeholder="Fill in the form" />
+      </StyledApp>
+    </>
   );
 }
 
