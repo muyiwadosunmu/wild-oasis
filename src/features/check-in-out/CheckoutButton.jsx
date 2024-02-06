fde0d955638e0c8373eb4a2a6a01e4da48516dd8
Dosunmu Oluwamuyiwa <oluwamuyiwadosunmu@gmail.com>
2024-02-06 13:17:15 +0100

    Setup Error boundaries

--- src/features/check-in-out/CheckoutButton.jsx
+++ src/features/check-in-out/CheckoutButton.jsx
@@ -1,8 +1,10 @@
 import Button from "../../ui/Button";
+import { useCheckout } from "./useCheckout";
 
 function CheckoutButton({ bookingId }) {
+  const { isCheckingOut, checkout } = useCheckout();
   return (
-    <Button variation="primary" size="small">
+    <Button variation="primary" size="small" onClick={checkout(bookingId)}>
       Check out
     </Button>
   );
