6ef49d892f6e843b9b80404c273dc446d2d38317
Dosunmu Oluwamuyiwa <oluwamuyiwadosunmu@gmail.com>
2024-02-06 11:42:51 +0100

    Displaying  Pie chart, NOT WORKING FOR ME

--- src/features/dashboard/SalesChart.jsx
+++ src/features/dashboard/SalesChart.jsx
@@ -93,7 +93,9 @@ function SalesChart({ bookings, numDays }) {
       };
   return (
     <StyledSalesChart>
-      <Heading as="h2"></Heading>Sales <Heading />
+      <Heading as="h2"></Heading>Sales from
+      {format(allDates.at(0), "MMM dd yyyy")} &mdash;{" "}
+      {format(allDates.at(-1), "MMM dd yyyy")} <Heading />
       <ResponsiveContainer height={300} width="100%">
         <AreaChart data={data}>
           <XAxis
